30796d7f4f2cf56c606bed53510770df44714f99
Egor Seleznev <79448643+egseleznev@users.noreply.github.com>
2022-01-04 19:46:59 +0300

    Update Server.cpp

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,6 +1,3 @@
-﻿// Melnicov_Server.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include "pch.h"
 #include "Message.h"
 #include "Session.h"
